c5473aa8f60c7b472f66c7b9eb05f39a62a1f24e
HoStWgSp <hostwg@vk.com>
2024-10-28 19:32:25 +1100

    Задание 29.3.7

--- JavaScript/Static/JS/index.js
+++ JavaScript/Static/JS/index.js
@@ -18,19 +18,19 @@ let contentFilter =  function filterContent(){
 * Будет вызываться благодаря атрибуту oninput на index.html
 *
 * */
-function filterContent(){
+function filterContent( userInput ){ // Принимаем пользовательский ввод в качестве параметра.
   
-   // Сохраняем текст пользовательского запроса.
-   let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
    // Находим контейнеры с видео, которые необходимо фильтровать
    let elements = document.getElementsByClassName('video-container');
  
    // Пробегаемся по контейнерам
    for (let i = 0; i <= elements.length; i++ ){
        // Вытаскиваем текст описания видео, которое необходимо отфильтровать
-       let videoText = elements[i].querySelector(".video-title").innerText;
+let videoText = elements[i].querySelector(".video-title").innerText;
+ 
+ 
        // Выполняем фильтрацию, сравнивая значения в нижнем регистре
-       if(!videoText.toLowerCase().includes(inputString.toLowerCase())){
+       if(!videoText.toLowerCase().includes(userInput.toLowerCase())){
            // Скрываем неподходящие
            elements[i].style.display = 'none';
        } else {
