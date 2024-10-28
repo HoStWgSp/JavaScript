55a9c73d21e68b9221f6f27b75090c3f99bbc533
HoStWgSp <hostwg@vk.com>
2024-10-28 20:45:03 +1100

    Задание 29.7.4

--- JavaScript/Startup.cs
+++ JavaScript/Startup.cs
@@ -27,21 +27,9 @@ namespace JavaScript
  
            app.UseEndpoints(endpoints =>
            {
-            //    endpoints.MapGet("/", async context =>
-            //    {
-            //        var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "index.html");
-                  
-            //        // Загружаем шаблон страницы, вставляя в него элементы
-            //        var html =  new StringBuilder(await File.ReadAllTextAsync(viewPath))
-            //            .Replace("<!--SIDEBAR-->", sideBarHtml)
-            //            .Replace("<!--FOOTER-->", footerHtml);
-                  
-            //        await context.Response.WriteAsync(html.ToString());
-            //    });
-              
                endpoints.MapGet("/", async context =>
                {
-                   var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "testing.html");
+                   var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "index.html");
                   
                    // Загружаем шаблон страницы, вставляя в него элементы
                    var html =  new StringBuilder(await File.ReadAllTextAsync(viewPath))
@@ -51,19 +39,43 @@ namespace JavaScript
                    await context.Response.WriteAsync(html.ToString());
                });
               
-               endpoints.MapGet("/Static/CSS/index.css", async context =>
-               {
+                endpoints.MapGet("/testing", async context =>
+                {
+                   var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "testing.html");
+                  
+                   // Загружаем шаблон страницы, вставляя в него элементы
+                   var html =  new StringBuilder(await File.ReadAllTextAsync(viewPath))
+                       .Replace("<!--SIDEBAR-->", sideBarHtml)
+                       .Replace("<!--FOOTER-->", footerHtml);
+                  
+                   await context.Response.WriteAsync(html.ToString());
+                });
+
+                endpoints.MapGet("/about", async context =>
+                {
+                    var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "about.html");
+                    
+                    // Загружаем шаблон страницы, вставляя в него элементы
+                    var html =  new StringBuilder(await File.ReadAllTextAsync(viewPath))
+                        .Replace("<!--SIDEBAR-->", sideBarHtml)
+                        .Replace("<!--FOOTER-->", footerHtml);
+                    
+                    await context.Response.WriteAsync(html.ToString());
+                });
+              
+                endpoints.MapGet("/Static/CSS/index.css", async context =>
+                {
                    var cssPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "CSS", "index.css");
                    var css = await File.ReadAllTextAsync(cssPath);
                    await context.Response.WriteAsync(css);
-               });
+                });
               
-               endpoints.MapGet("/Static/JS/index.js", async context =>
-               {
+                endpoints.MapGet("/Static/JS/index.js", async context =>
+                {
                    var jsPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "JS", "index.js");
                    var js = await File.ReadAllTextAsync(jsPath);
                    await context.Response.WriteAsync(js);
-               });
+                });
            });
        }
     }
