82f2aca90ca6ee31eedb10350284c727ee22802b
HoStWgSp <hostwg@vk.com>
2024-10-28 19:44:14 +1100

    Задание 29.4.4

--- JavaScript/Static/JS/index.js
+++ JavaScript/Static/JS/index.js
@@ -24,23 +24,15 @@ let sessionLog = function logSession(session) {
 * Будет вызываться благодаря атрибуту oninput на index.html
 *
 * */
-function filterContent( userInput ){ // Принимаем пользовательский ввод в качестве параметра.
+function filterContent( inputParseFunction ){ // в этот параметр будет передана ФУНКЦИЯ парсинга пользовательского ввода
   
-   // Находим контейнеры с видео, которые необходимо фильтровать
    let elements = document.getElementsByClassName('video-container');
  
-   // Пробегаемся по контейнерам
    for (let i = 0; i <= elements.length; i++ ){
-       // Вытаскиваем текст описания видео, которое необходимо отфильтровать
-let videoText = elements[i].querySelector(".video-title").innerText;
- 
- 
-       // Выполняем фильтрацию, сравнивая значения в нижнем регистре
-       if(!videoText.toLowerCase().includes(userInput.toLowerCase())){
-           // Скрываем неподходящие
+       let videoText = elements[i].querySelector(".video-title").innerText;
+       if(!videoText.toLowerCase().includes(inputParseFunction() /*Переданная функция вызвана*/  .toLowerCase())){
            elements[i].style.display = 'none';
        } else {
-           // Показываем подходящие
            elements[i].style.display = 'inline-block';
        }
    }
