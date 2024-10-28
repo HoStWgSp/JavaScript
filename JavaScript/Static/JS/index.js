7f2263c354a7ffb06c79ddb53cba8ecfb0c0559f
HoStWgSp <hostwg@vk.com>
2024-10-28 19:57:58 +1100

    Задание 29.5.5

--- JavaScript/Static/JS/index.js
+++ JavaScript/Static/JS/index.js
@@ -1,3 +1,16 @@
+let session =  new Map();
+/*
+* Сохранение данных сессии сразу при заходе пользователя на страницу
+* 
+* */
+function handleSession(){
+   // Сохраним время начала сессии
+   session.set("startDate", new Date().toLocaleString())
+   // Сохраним UserAgent
+   session.set("userAgent", window.navigator.userAgent)
+}
+
+
 // Сохраняем функцию 1  в переменную
 let contentFilter =  function filterContent(){
    let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
@@ -12,9 +25,11 @@ let contentFilter =  function filterContent(){
        }
    }
 }
-// Логирование сессии (объявлено через expression)
-let sessionLog = function logSession(session) {
-   // Вывод в консоль
+/*
+* Вывод данных сессии в консоль
+* 
+* */
+let sessionLog = function logSession() {
    for (let result of session){
        console.log(result)
    }
@@ -37,28 +52,18 @@ function filterContent(){
     }
 }
 
-// Обработка сессии (объявлено через declaration)
-function handleSession(){
-   // создадим объект Map для хранения сессии
-   let session =  new Map();
-   // Сохраним UserAgent
-   session.set("userAgent", window.navigator.userAgent)
-  
-   // Запросим возраст пользователя и тоже сохраним
+/*
+* Проверка возраста пользователя
+* 
+* */
+function checkAge(){
    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
-  
-   // Проверка на возраст и сохранение сессии
+   
    if(session.get("age") >= 18){
-       let startDate = new Date().toLocaleString();
-      
-       alert("Приветствуем на LifeSpot! " + '\n' +  "Текущее время: " + startDate );
-       session.set("startDate", startDate)
+       alert("Приветствуем на LifeSpot! " + '\n' +  "Текущее время: " + new Date().toLocaleString() );
    }
    else{
        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
        window.location.href = "http://www.google.com"
    }
-  
-   // Теперь мы возвращаем объект сессии
-   return session;
 }
