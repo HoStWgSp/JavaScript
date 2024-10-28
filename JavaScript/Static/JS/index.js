9e721be67acfea9762bacad955004c5b8c70903f
HoStWgSp <hostwg@vk.com>
2024-10-28 18:44:15 +1100

    Задание 29.2.5

--- JavaScript/Static/JS/index.js
+++ JavaScript/Static/JS/index.js
@@ -1,3 +1,18 @@
+// Сохраняем функцию 1  в переменную
+let contentFilter =  function filterContent(){
+   let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
+   let elements = document.getElementsByClassName('video-container');
+ 
+   for (let i = 0; i <= elements.length; i++ ){
+       let videoText = elements[i].querySelector(".video-title").innerText;
+       if(!videoText.toLowerCase().includes(inputString.toLowerCase())){
+           elements[i].style.display = 'none';
+       } else {
+           elements[i].style.display = 'inline-block';
+       }
+   }
+}
+
 /*
 * Функция для фильтрации контента
 * Будет вызываться благодаря атрибуту oninput на index.html
@@ -25,23 +40,12 @@ function filterContent(){
    }
 }
 
-/*
-* Функция для проверки и сохранения  данных пользователя
-* Также блокирует доступ к сайту лицам, не подтвердившим свой возраст
-*
-* */
- 
-function handleSession(){
-  
-   // создадим объект Map для хранения сессии
+// Сохраняем функцию 2 в переменную
+let sessionHandler = function handleSession(){
    let session =  new Map();
-   // Сохраним UserAgent
    session.set("userAgent", window.navigator.userAgent)
+   session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
   
-   // Запросим возраст пользователя и тоже сохраним
-   session.set("age", prompt("Пожалуйста, введите ваш возраст"))
-  
-   // Проверка на возраст и сохранение сессии
    if(session.get("age") >= 18){
        let startDate = new Date().toLocaleString();
       
@@ -51,8 +55,8 @@ function handleSession(){
    else{
        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
        window.location.href = "http://www.google.com"
-   }  
-   // Вывод в консоль
+   }
+  
    for (let result of session){
        console.log(result)
    }
